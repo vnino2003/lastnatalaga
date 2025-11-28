187833024cf707f063c65d64233038a8417cc870
vnino2003 <vincent.edu.mail@gmail.com>
2025-11-28 11:52:49 +0800

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -116,7 +116,7 @@ $config['log_dir']                  = 'runtime/logs/';
 | Enabling this setting will tell LavaLust to look for a Composer
 | package auto-loader script in app/vendor/autoload.php.
 |
-|	$config['composer_autoload'] = TRUE;
+|	$config['composer_autoload'] = FALSE;
 |
 | Or if you have your vendor/ directory located somewhere else, you
 | can opt to set a specific path as well:
