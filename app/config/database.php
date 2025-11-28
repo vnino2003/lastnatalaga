e47369b9f611776b31c8c4dbefd63b8729134aa2
vnino2003 <vincent.edu.mail@gmail.com>
2025-11-28 09:48:01 +0800

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
    'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
+    'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'finalproject',
+    'username'	=> 'sql12809699',
+    'password'	=> 'XhYYI64GGc',
+    'database'	=> 'sql12809699',
     'charset'	=> 'utf8',
     'dbprefix'	=> '',
     // Optional for SQLite
