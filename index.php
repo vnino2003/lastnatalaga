98c9ef24fe54940a66ffa6ad6e52735c47fbf950
vnino2003 <vincent.edu.mail@gmail.com>
2025-11-28 10:35:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -84,7 +84,7 @@ define('PUBLIC_DIR', $public_folder);
  * Setup done? Then Hurray!
  * ------------------------------------------------------
  */
-require_once ROOT_DIR . 'vendor/autoload.php';
+// require_once ROOT_DIR . 'vendor/autoload.php';
 
 require_once SYSTEM_DIR . 'kernel/LavaLust.php';
 ?>
