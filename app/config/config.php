d88c02edbb8a482dfd3c646f1381253d68392ad9
vnino2003 <vincent.edu.mail@gmail.com>
2025-11-30 22:06:54 +0800

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -80,7 +80,7 @@ $config['ENVIRONMENT'] = 'production';
 // |$config['base_url'] 				= 'http://localhost/finalProject_SIA_WEB/';
 
 */
-$config['base_url'] 				= 'https://mocart.onrender.com/';
+$config['base_url'] 				= 'https://lastnatalaga.onrender.com/';
 /*
 |--------------------------------------------------------------------------
 | Index File
