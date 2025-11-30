cee30d3802e35dc15307821b7967d301c6c5e9d1
vnino2003 <vincent.edu.mail@gmail.com>
2025-11-30 23:23:35 +0800

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -64,7 +64,7 @@ $config['VERSION']                 = '4.4.0';
 | -------------------------------------------------------------------
 | Values: development and production
 */
-$config['ENVIRONMENT'] = 'production';
+$config['ENVIRONMENT'] = 'development';
 
 /*
 |--------------------------------------------------------------------------
@@ -90,7 +90,7 @@ $config['base_url'] 				= 'https://mocart.onrender.com/';
 | variable to blank.
 |
 */
-$config['index_page']               = '';
+$config['index_page']               = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
