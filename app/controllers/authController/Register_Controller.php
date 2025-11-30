ce88f9eabe115538aff205c2be8ad12a880d6d29
vnino2003 <vincent.edu.mail@gmail.com>
2025-11-30 16:09:37 +0800

    Update Register_Controller.php

--- app/controllers/authController/Register_Controller.php
+++ app/controllers/authController/Register_Controller.php
@@ -130,7 +130,7 @@ public function googleRedirect() {
     $client = new GoogleClient();
     $client->setClientId('667097836235-o068b667r2df513mqik97v5ch4csngf4.apps.googleusercontent.com');
     $client->setClientSecret('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');
-    $client->setRedirectUri('http://localhost/finalProject_SIA_WEB/register/googleCallback');
+    $client->setRedirectUri('https://mocart.onrender.com/register/googleCallback');
     $client->addScope('email');
     $client->addScope('profile');
 
@@ -146,7 +146,7 @@ public function googleCallback() {
     $client = new GoogleClient();
     $client->setClientId('667097836235-o068b667r2df513mqik97v5ch4csngf4.apps.googleusercontent.com');
     $client->setClientSecret('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');
-    $client->setRedirectUri('http://localhost/finalProject_SIA_WEB/register/googleCallback');
+    $client->setRedirectUri('https://mocart.onrender.com/register/googleCallback');
 
     if (isset($_GET['code'])) {
 
