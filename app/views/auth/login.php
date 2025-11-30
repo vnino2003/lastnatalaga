83c5a51225c5f65379a45dfd685c4c09ca2a48d0
vnino2003 <vincent.edu.mail@gmail.com>
2025-11-30 10:56:32 +0800

    Update login.php

--- app/views/auth/login.php
+++ app/views/auth/login.php
@@ -107,7 +107,7 @@
                                 <input type="password" name="password" class="form-control" placeholder="Your Password">
                             </div>
                             <div class="form-group mt-3">
-    <div class="g-recaptcha" data-sitekey="6LeZYPorAAAAAP0iVvg7MwcJ7YV8gmtxvl8F4TLV"></div>
+    <div class="g-recaptcha" data-sitekey="6LcscBwsAAAAABHmMbyJ6sV2yTZek9DgJxas9uie"></div>
 </div>
                             <div class="d-flex justify-content-between mb-4">
                                 <div class="form-check">
